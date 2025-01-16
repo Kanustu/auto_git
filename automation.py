b410a302bbd1885587a19fcb0df8c6340e06718e
JDKanius <kanustu@gmail.com>
2025-01-16 13:29:18 -0600

    daily update completed on 2025-01-16 13:29:17.966631

--- automation.py
+++ automation.py
@@ -1,14 +1,36 @@
+from git import Repo, exc
 import os
 import subprocess
 from datetime import datetime
 import logging
 import sys
 
+local_repo = "/Users/jordankanius/automation/automated_update"
+commit_message = f"daily update completed on {datetime.now()}"
 
 def current_date():
     file_path = "date.txt"
     content = f"File updated on {datetime.now()}"
     with open(file_path, 'w') as file:
         file.write(content)
+    print("file updated successfully")
+
+
+def git_upload():
+    repo = Repo(local_repo)
+    if repo.is_dirty():
+        repo.git.add('--all')
+        print('added')
+    
+        repo.index.commit(commit_message)
+        print('commited changes')
+
+        origin = repo.remote(name='origin')
+        origin.push()
+        print('changes pushed')
+    else:
+        print('no changes to push')
+       
 
 current_date()
+git_upload()
